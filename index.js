2cc8b150087851b0fd26cac1bd750a9d1fc4717c
MikeHuijgen <mikeh162001@hotmail.com>
2021-12-03 15:02:32 +0100

    les 7 afgemaakt en functions gemaakt

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
-class player
+class Player
 {
     constructor(index)
     {
         this.index = index;
         this.atTile = 0;
-        this.pawn = document.getElementsByName("pawn" + index)[0];
+        this.pawn = document.getElementsByClassName("pawn" + index)[0];
         this.pawn.style.display = "block";
     }
 }
 
-class tile
+class Tile
 {
     constructor(div)
     {
@@ -18,20 +18,69 @@ class tile
     }
 }
 
-class game
+class Game
 {
     constructor()
     {
-        this.selectplayerDiv = document.getElementsByName("selectplayers");
-        this.winnerDiv = document.getElementsByName("winner")[0];
-        this.playerturnDiv = document.getElementsByName("playerturn")[0];
-        this.rollDiv = document.getElementsByName("roll")[0];
-        this.mainDiv = document.getElementsByName("main")[0];
-        this.boardDiv = document.getElementsByName("board")[0];
-        this.boardoverlayDiv = document.getElementsByName("boardoverlay")[0];
+        this.selectplayerDiv = document.getElementsByClassName("selectplayers")[0];
+        this.winnerDiv = document.getElementsByClassName("winner")[0];
+        this.playerturnDiv = document.getElementsByClassName("playerturn")[0];
+        this.rollDiv = document.getElementsByClassName("roll")[0];
+        this.mainDiv = document.getElementsByClassName("main")[0];
+        this.boardDiv = document.getElementsByClassName("board")[0];
+        this.boardoverlayDiv = document.getElementsByClassName("boardoverlay")[0];
 
-        this.tile = [];
+        this.tiles = [];
         this.player = [];
-        this.playerturn = [];
+        this.playerturn = 0;
     }
+
+    setupBoard()
+    {
+
+    }
+
+    setupGotos()
+    {
+
+    }
+
+    start(amountOfPlayers)
+    {
+
+    }
+
+    moveToNextPlayer()
+    {
+
+    }
+
+    draw()
+    {
+
+    }
+
+    roll()
+    {
+
+    }
+
+    setPawn()
+    {
+
+    }
+
+    makeBoardDiv(x,y,tileDisplayNumber)
+    {
+
+    }
+
 }
+
+function init()
+{
+    game = new Game ();
+}
+
+let game;
+
