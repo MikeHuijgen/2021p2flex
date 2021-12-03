ff2af10c58d8e4a71a56815f9cc5ffde621d60a7
MikeHuijgen <mikeh162001@hotmail.com>
2021-12-03 13:56:08 +0100

    Classes Player Game Tile gemaakt

--- index.js
+++ index.js
@@ -1,17 +1,37 @@
 class player
 {
-
+    constructor(index)
+    {
+        this.index = index;
+        this.atTile = 0;
+        this.pawn = document.getElementsByName("pawn" + index)[0];
+        this.pawn.style.display = "block";
+    }
 }
 
 class tile
 {
     constructor(div)
     {
-        
+        this.div = div;
+        this.goto = -1;
     }
 }
 
 class game
 {
+    constructor()
+    {
+        this.selectplayerDiv = document.getElementsByName("selectplayers");
+        this.winnerDiv = document.getElementsByName("winner")[0];
+        this.playerturnDiv = document.getElementsByName("playerturn")[0];
+        this.rollDiv = document.getElementsByName("roll")[0];
+        this.mainDiv = document.getElementsByName("main")[0];
+        this.boardDiv = document.getElementsByName("board")[0];
+        this.boardoverlayDiv = document.getElementsByName("boardoverlay")[0];
 
+        this.tile = [];
+        this.player = [];
+        this.playerturn = [];
+    }
 }
