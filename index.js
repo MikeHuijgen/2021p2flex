5022de17d0bea25b8a484a4296620b4c02e76063
MikeHuijgen <mikeh162001@hotmail.com>
2021-12-09 13:40:10 +0100

    Les 9 gemaakt bord gemaakt

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
+function init()
+{
+    game = new Game ();
+}
+
+let game;
+
 class Player
 {
     constructor(index)
@@ -33,11 +40,45 @@ class Game
         this.tiles = [];
         this.player = [];
         this.playerturn = 0;
+        this.setupBoard();
     }
 
     setupBoard()
     {
+        let path = [
+            1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 3, 3, 3,
+            3, 3, 3, 3, 3, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 3,
+            3, 3,3, 3, 3, 3, 3, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1
+        ]
+        
+        let x = 0;
+        let y = 10;
+        let tileSize = 55;
+         
+        for (var i = 0; i < path.length; i++)
+        {
+            let cmd = path[i];
+            if (cmd == 1)
+            {
+                //right
+                x++;
+            }
+            else if (cmd == 3)
+            {
+                //left
+                x--;
+            }
+            else if (cmd == 0)
+            {
+                //up
+                y--;
+            }
 
+            let div = this.makeBoardDiv(x * tileSize, y * tileSize,i+1)
+
+            let tile = new Tile(div);
+            this.tiles.push(tile);
+        }
     }
 
     setupGotos()
@@ -72,15 +113,19 @@ class Game
 
     makeBoardDiv(x,y,tileDisplayNumber)
     {
+        let div = document.createElement("div");
 
+        div.className = "tile";
+        div.style.left = x + "px";
+        div.style.top = y + "px";
+        div.textContent = tileDisplayNumber;
+
+        this.boardDiv.appendChild(div);
+
+        return div;
     }
 
 }
 
-function init()
-{
-    game = new Game ();
-}
 
-let game;
 
